406c52b646f159a73210562e89171b6156c249e0
fahad <sohelcse16@gmail.com>
2022-10-25 19:12:07 +0600

    index file correction

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require("./data/courses.json");
-// app.get("/", (req, res) => {
-//   res.send(`Sever running on port:${port}`);
-// });
+
 app.get("/", (req, res) => {
   res.send(courses);
 });
@@ -19,6 +17,7 @@ app.get("/courses/:id", (req, res) => {
   const course = courses.find((course) => course.id === id);
   res.send(course);
 });
+
 app.listen(port, () => {
   console.log(`port running ${port}`);
 });
